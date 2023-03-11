9c368b6fa40d090790358ea122d16fa33b44c232
Igor Vereshchagin <9868425@gmail.com>
2023-03-11 18:04:35 +0300

    feat: make functions like the methods of object

--- js/examples.js
+++ js/examples.js
@@ -115,17 +115,151 @@
 
 // thirdTask();
 
-const lines = 5;
-let result = '';
+// const lines = 5;
+// let result = '';
 
-for (let i = 0; i <= lines; i++) {
-    for (let j = 0; j < lines - i; j++) {
-        result += " ";
-    }
-    for (let j = 0; j < 2 * i + 1; j++) {
-        result += "*";
-    }
-    result += "\n";
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result);
+
+// function copy (mainObject) {
+//     const copyObject = {};
+
+//     for (let key in mainObject) {
+//         copyObject[key] = mainObject[key];
+//     }
+
+//     return copyObject;
+// }
+
+// const startObject = {
+//     a: 1,
+//     b: 2,
+//     c: {
+//         x: 1,
+//         y: 2
+//     }
+// }
+
+// const finishObject = copy(startObject);
+
+// finishObject.a = 10;
+// finishObject.c.x = 20;
+
+// const addObject = {
+//     v: 30,
+//     i: 45
+// }
+
+// const thirdObject = Object.assign({}, addObject);
+
+// console.log(thirdObject);
+
+// console.log(finishObject);
+// console.log(startObject);
+
+// const arr = [1, 2, 3, 4];
+
+// const newArr = arr.slice();
+
+// arr[0] = 100;
+
+// console.log(arr);
+// console.log(newArr);
+
+// const personalPlanPeter = {
+//     name: "Peter",
+//     age: "29",
+//     skills: {
+//         languages: ['ru', 'eng'],
+//         programmingLangs: {
+//             js: '20%',
+//             php: '10%'
+//         },
+//         exp: '1 month'
+//     },
+//     showAgeAndLangs: function(personalPlanPeter) {
+//         const { age } = personalPlanPeter;
+//         const { languages } = personalPlanPeter.skills;
+//         let str = `Мне ${age} и я владею языками: ${languages.toUpperCase()}`;
+
+
+//         return str;
+//     }
+// };
+
+// function showExperience(plan) {
+//     const {exp} = plan.skills;
+//     console.log(exp);
+// }
+
+// showExperience(personalPlanPeter);
+
+// function showProgrammingLangs(plan) {
+//     let str = '';
+//     const {programmingLangs} = plan.skills;
+//     for(let key in programmingLangs) {
+//         str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+//     }
+//     console.log(str);
+// }
+
+// showProgrammingLangs(personalPlanPeter);
+
+// const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+// function showFamily(arr) {
+//     if(arr.length == 0) {
+//         console.log('Семья пуста');
+//     } else {
+//         const familyInText = arr.slice(',').join(' ');
+//         console.log(`Семья состоит из: ${familyInText}`);
+//     }
+// }
+
+// showFamily(family);
+
+// const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+// function standardizeStrings(arr) {
+//     console.log(arr.join('\n').toLowerCase());
+// }
+
+// standardizeStrings(favoriteCities);
+
+// const someString = 'This is some strange string';
+
+// function reverse(str) {
+//     if(typeof str != 'string') {
+//         console.log('Ошибка!');
+//     } else {
+//         const reverseString = str.split('').reverse().join('');
+//         console.log(reverseString);
+//     }
+// }
+
+// reverse(someString);
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    let str = '';
+    arr.length == 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+    arr.forEach(element => {
+        if(element !== missingCurr) {
+            str += `${element}\n`;
+        }
+    });
+    console.log(str);
 }
 
-console.log(result);
+availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY');
